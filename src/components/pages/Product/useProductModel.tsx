1ff5428de91bbb4b974e920090b5d17bba88b7a0
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-06-10 11:53:17 +0600

    seo

--- src/components/pages/Product/useProductModel.tsx
+++ src/components/pages/Product/useProductModel.tsx
@@ -3,11 +3,14 @@ import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { useCallback, useEffect } from 'react';
 import { products } from 'store';
 import { getProductThunk } from 'store/products/thunks';
+import { useHydrateState } from 'shared/hooks';
 
 export function useProductModel({ id }: { id: string }) {
   const dispatch = useAppDispatch();
   const getProductState = useAppSelector(products.getProduct.selector.state);
   const model = getProductState.data;
+
+  const isHydratedToClient = useHydrateState();
 
   const getProduct = useCallback(
     (id: string) => dispatch(getProductThunk(id)),
@@ -15,14 +18,20 @@ export function useProductModel({ id }: { id: string }) {
   );
 
   useEffect(() => {
+    if (!isHydratedToClient) return;
+
     if (id) {
       getProduct(id);
-
-      return () => {
-        dispatch(products.getProduct.action.reset());
-      };
     }
-  }, [id, getProduct, dispatch]);
+  }, [id, getProduct, dispatch, isHydratedToClient]);
+
+  useEffect(() => {
+    return () => {
+      if (!isHydratedToClient) return;
+
+      dispatch(products.getProduct.action.reset());
+    };
+  }, [dispatch, isHydratedToClient]);
 
   return {
     model,
