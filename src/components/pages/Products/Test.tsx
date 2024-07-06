6f55b94b5f708480b2597d959a418ae1550491b3
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-07-06 11:09:39 +0600

    array

--- src/components/pages/Products/Test.tsx
+++ src/components/pages/Products/Test.tsx
@@ -67,6 +67,10 @@ export default function App() {
       .messages({ 'string.pattern.base': 'bad format' }),*/
     //title: Joi.array().items(Joi.number().valid(2)).required(),
     //title: Joi.array().items(Joi.number(), Joi.string()).required(),
+    person: {
+      firstName: Joi.string().label('First name'),
+      lastName: Joi.string().label('Last name'),
+    },
     cart: Joi.any(),
   });
 
