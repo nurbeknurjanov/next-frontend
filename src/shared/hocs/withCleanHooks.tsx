d546b546539d8794e37e3e8bd7bdc5b552cdc22f
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-07-14 11:57:08 +0600

    cookiesfinsih

--- src/shared/hocs/withCleanHooks.tsx
+++ src/shared/hocs/withCleanHooks.tsx
@@ -1,13 +1,17 @@
 import React, { FC, ComponentType, useEffect } from 'react';
 import { common } from 'store';
 import { hydratedToClient } from 'store/common/thunks';
-import { useAppDispatch } from 'store/hooks';
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { useCookies } from 'react-cookie';
+import { getIsAuth } from 'store/common/selectors';
 
 export const withCleanHooks = <T extends object>(
   Component: ComponentType<T>
 ) => {
   const NewComponent: FC<T> = props => {
     const dispatch = useAppDispatch();
+    const isAuth = useAppSelector(getIsAuth);
+    const [, , removeCookie] = useCookies(['refreshToken', 'accessToken']);
 
     useEffect(
       () => () => {
@@ -21,6 +25,13 @@ export const withCleanHooks = <T extends object>(
       },
       [dispatch]
     );
+
+    useEffect(() => {
+      if (!isAuth) {
+        removeCookie('refreshToken');
+        removeCookie('accessToken');
+      }
+    }, [removeCookie, isAuth]);
 
     return <Component {...props} />;
   };
