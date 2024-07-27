8b7c1dcb8f7c7ebfd4b82820a1849c50981075ca
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-07-27 12:30:54 +0600

    profile

---
+++ src/components/pages/Profile/components/ProfileModalChangePassword/ProfileModalChangePassword.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useProfileModalChangePassword } from './useProfileModalChangePassword';
+import { TextField } from '@mui/material';
+import { Button } from 'shared/ui';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useRef } from 'react';
+
+export type IProps = {
+  id: string;
+  onClose: () => void;
+};
+
+export const ProfileModalChangePassword: React.FC<IProps> = ({
+  onClose,
+  id,
+}) => {
+  //const id = type === 'update' ? props.id : null;
+  const formRef = useRef<HTMLFormElement>();
+  //const formRef = useRef<HTMLFormElement>(null); //for direct assign
+  const {
+    tCommon,
+    tUserPage,
+    tUser,
+    userChangePasswordState,
+    register,
+    errors,
+    isValid,
+    isDirty,
+    handleSubmit,
+    submitForm,
+  } = useProfileModalChangePassword({
+    id: id!,
+    onClose,
+  });
+
+  return (
+    <Dialog open onClose={onClose}>
+      <DialogTitle>{tUserPage('changePassword')}</DialogTitle>
+      <DialogContent>
+        <form
+          ref={el => (formRef.current = el!)}
+          /*ref={formRef}*/
+          onSubmit={e => {
+            e.preventDefault();
+            handleSubmit(submitForm)(e);
+          }}
+        >
+          <TextField
+            label={tUser('password')}
+            error={!!errors['password']}
+            helperText={errors['password']?.message}
+            {...register('password')}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{tCommon('close')}</Button>
+        <Button
+          variant={'contained'}
+          onClick={() => {
+            formRef.current?.requestSubmit();
+          }}
+          disabled={!isDirty || !isValid}
+          autoFocus
+          loading={userChangePasswordState.isFetching}
+          sx={{ minWidth: 120 }}
+        >
+          {tCommon('update')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
---
+++ src/components/pages/Profile/components/ProfileModalChangePassword/useProfileModalChangePassword.ts
@@ -0,0 +1,62 @@
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { users } from 'store';
+import { useTranslations } from 'next-intl';
+import { IUserPost } from 'api/usersApi';
+import { IProps } from './ProfileModalChangePassword';
+import { notify } from 'store/common/thunks';
+import { userChangePasswordThunk } from 'store/users/thunks';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { useI18nJoi } from 'shared/utils';
+import Joi from 'joi';
+
+export function useProfileModalChangePassword({ onClose, id }: IProps) {
+  const dispatch = useAppDispatch();
+  const tCommon = useTranslations('Common');
+  const tUserPage = useTranslations('UserPage');
+  const tUser = useTranslations('User');
+
+  const userChangePasswordState = useAppSelector(
+    users.userChangePassword.selector.state
+  );
+
+  const i18nJoi = useI18nJoi();
+  const schema = i18nJoi.object({
+    password: Joi.string().min(6).label(tUser('password')),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isDirty },
+  } = useForm<IUserPost>({
+    mode: 'onTouched',
+    resolver: joiResolver(schema),
+  });
+
+  const updateUserPassword = async (id: string, formData: IUserPost) => {
+    const { data } = await dispatch(userChangePasswordThunk(id, formData));
+
+    if (data) {
+      onClose();
+      dispatch(notify(tCommon('successUpdated'), 'success'));
+    }
+  };
+
+  const submitForm = (formData: IUserPost) => {
+    updateUserPassword(id!, formData);
+  };
+
+  return {
+    tCommon,
+    tUserPage,
+    tUser,
+    userChangePasswordState,
+    register,
+    errors,
+    isValid,
+    isDirty,
+    handleSubmit,
+    submitForm,
+  };
+}
