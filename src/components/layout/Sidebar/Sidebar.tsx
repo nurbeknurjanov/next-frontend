06e4f966b2b198fb90d0f5a9945fb239aae7ba24
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-07-30 14:51:02 +0600

    sidebar

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,89 +1,14 @@
 'use client';
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { ActiveLink } from 'shared/ui';
 import styles from './siderbar.module.scss';
-import { to } from 'shared/utils';
-import { useAppSelector } from 'store/hooks';
-import { getIsAuth } from 'store/common/selectors';
-import { useTranslations } from 'next-intl';
-import { useRouter } from 'next/navigation';
-import CategoryIcon from '@mui/icons-material/Category';
-import GroupIcon from '@mui/icons-material/Group';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import PhoneIcon from '@mui/icons-material/Phone';
-import HomeIcon from '@mui/icons-material/Home';
+import { ListMenu } from './components';
 
 export function Sidebar() {
-  const tCommon = useTranslations('Common');
-  const tProductsPage = useTranslations('ProductsPage');
-  const tUsersPage = useTranslations('UsersPage');
-  const tFilesPage = useTranslations('FilesPage');
-  const tContactPage = useTranslations('ContactPage');
-  const router = useRouter();
-  const isAuth = useAppSelector(getIsAuth);
   return (
     <section className={styles.sidebar}>
       <Paper sx={{ height: '100%' }}>
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton component={ActiveLink} href={to({ pathname: '/' })}>
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText primary={tCommon('home')} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component={ActiveLink} href="/products">
-              <ListItemIcon>
-                <CategoryIcon />
-              </ListItemIcon>
-              <ListItemText primary={tProductsPage('title')} />
-            </ListItemButton>
-          </ListItem>
-          {isAuth && (
-            <>
-              <ListItem disablePadding>
-                <ListItemButton component={ActiveLink} href="/users">
-                  <ListItemIcon>
-                    <GroupIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={tUsersPage('title')} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={ActiveLink} href="/files">
-                  <ListItemIcon>
-                    <AttachFileIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={tFilesPage('title')} />
-                </ListItemButton>
-              </ListItem>
-            </>
-          )}
-          <ListItem disablePadding>
-            <ListItemButton
-              component={ActiveLink}
-              href={to({ pathname: '/contact', search: { fromSidebar: '1' } })}
-              onClick={() =>
-                router.push(
-                  to({ pathname: '/contact', search: { fromSidebar: '2' } })
-                )
-              }
-            >
-              <ListItemIcon>
-                <PhoneIcon />
-              </ListItemIcon>
-              <ListItemText primary={tContactPage('title')} />
-            </ListItemButton>
-          </ListItem>
-        </List>
+        <ListMenu />
       </Paper>
     </section>
   );
---
+++ src/components/layout/Sidebar/components/ListMenu.tsx
@@ -0,0 +1,86 @@
+'use client';
+import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { ActiveLink } from 'shared/ui';
+import { to } from 'shared/utils';
+import { useAppSelector } from 'store/hooks';
+import { getIsAuth } from 'store/common/selectors';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
+import CategoryIcon from '@mui/icons-material/Category';
+import GroupIcon from '@mui/icons-material/Group';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import PhoneIcon from '@mui/icons-material/Phone';
+import HomeIcon from '@mui/icons-material/Home';
+
+export function ListMenu() {
+  const tCommon = useTranslations('Common');
+  const tProductsPage = useTranslations('ProductsPage');
+  const tUsersPage = useTranslations('UsersPage');
+  const tFilesPage = useTranslations('FilesPage');
+  const tContactPage = useTranslations('ContactPage');
+  const router = useRouter();
+
+  const isAuth = useAppSelector(getIsAuth);
+
+  return (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton component={ActiveLink} href={to({ pathname: '/' })}>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary={tCommon('home')} />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component={ActiveLink} href="/products">
+          <ListItemIcon>
+            <CategoryIcon />
+          </ListItemIcon>
+          <ListItemText primary={tProductsPage('title')} />
+        </ListItemButton>
+      </ListItem>
+      {isAuth && (
+        <>
+          <ListItem disablePadding>
+            <ListItemButton component={ActiveLink} href="/users">
+              <ListItemIcon>
+                <GroupIcon />
+              </ListItemIcon>
+              <ListItemText primary={tUsersPage('title')} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component={ActiveLink} href="/files">
+              <ListItemIcon>
+                <AttachFileIcon />
+              </ListItemIcon>
+              <ListItemText primary={tFilesPage('title')} />
+            </ListItemButton>
+          </ListItem>
+        </>
+      )}
+      <ListItem disablePadding>
+        <ListItemButton
+          component={ActiveLink}
+          href={to({ pathname: '/contact', search: { fromSidebar: '1' } })}
+          onClick={() =>
+            router.push(
+              to({ pathname: '/contact', search: { fromSidebar: '2' } })
+            )
+          }
+        >
+          <ListItemIcon>
+            <PhoneIcon />
+          </ListItemIcon>
+          <ListItemText primary={tContactPage('title')} />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+}
