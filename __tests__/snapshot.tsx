212c1c34393cf31b5aebfda0179589238751f5bd
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-02 18:51:33 +0600

    homedummy

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,12 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-//import React from 'react';
-//import { render } from '@testing-library/react';
-//import { HomeDummy } from 'components/pages/HomeDummy';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { HomeDummy } from 'components/pages/HomeDummy';
 
-xit('renders homepage unchanged', () => {
-  /*const { container } = render(<HomeDummy />);
-  expect(container).toMatchSnapshot();*/
-  expect(1).toBe(1);
+it('renders homepage unchanged', () => {
+  const { container } = render(<HomeDummy />);
+  expect(container).toMatchSnapshot();
 });
