c407af83c3ef9e84063c419a97c2d783933a2991
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-02 16:18:08 +0600

    yarn

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,35 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { HomeDummy } from 'components/pages/HomeDummy';
+import { StoreProvider } from 'shared/wrappers/StoreProvider';
+import { TranslationsProvider } from 'shared/wrappers/TranslationsProvider';
+
+jest.mock('next-intl', () => {
+  const originalModule = jest.requireActual('next-intl');
+  return {
+    __esModule: true,
+    ...originalModule,
+    //default: () => 'mocked baz',
+    //NextIntlClientProvider: originalModule.NextIntlClientProvider,
+    useMessages: () => ({
+      HomePage: { title: 'Welcome', description: 'This is welcome page' },
+    }),
+  };
+});
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(
+      <TranslationsProvider>
+        <HomeDummy />
+      </TranslationsProvider>,
+      { wrapper: StoreProvider }
+    );
+
+    const heading = screen.getByRole('heading', { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Welcome');
+  });
+});
---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { HomeDummy } from 'components/pages/HomeDummy';
+
+xit('renders homepage unchanged', () => {
+  const { container } = render(<HomeDummy />);
+  expect(container).toMatchSnapshot();
+});
