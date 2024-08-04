d33ad76dab6a90ebf7b747c1f5885c0448392ff5
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-04 19:53:00 +0600

    jest_Works_finally3

--- jest.config.ts
+++ jest.config.ts
@@ -193,8 +193,6 @@ const config: Config = {
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  //transformIgnorePatterns: [`.*/node_modules/(?!(${esModules})/)`],
-
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
