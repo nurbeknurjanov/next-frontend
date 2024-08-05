6838b21d32a12a10e219f313bc42aed815f27164
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-05 19:55:56 +0600

    click

--- __tests__/Products.test.tsx
+++ __tests__/Products.test.tsx
@@ -1,18 +1,26 @@
 import '@testing-library/jest-dom';
 import { renderWithProviders } from 'tests';
 import React from 'react';
-import { screen, waitFor /*, act */ } from '@testing-library/react';
+import { screen, waitFor, fireEvent /*, act */ } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Products } from 'components/pages/Products';
 
 describe('Home', () => {
   it('renders a heading', async () => {
+    const user = userEvent.setup();
+
     const { container, getByText: _getByText } = renderWithProviders(
       <Products />
     );
 
-    //expect(container).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
     await waitFor(() => container.querySelector('.MuiCircularProgress-root'));
     await screen.findByText('Product 1');
+    const inputFilterName = screen.getAllByLabelText('Name')[0];
+    await user.type(inputFilterName, 'Product 5');
+    const submitButton = screen.getByRole('button', { name: 'Search' });
+    //fireEvent.change(inputFilterName, { target: { value: 'Product 5' } });
+    //fireEvent.click(screen.getByText(/click me/i))
     //const Product1 = await waitFor(() => getByText('Product 1'));
     /*await act(async () => {
       await new Promise(resolve => setTimeout(resolve, 1000));
