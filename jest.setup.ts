61aad3809b88285e5b05a5e182a6f25c5f98b103
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-05 17:36:27 +0600

    jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,13 @@
 import '@testing-library/jest-dom';
 /*import { TextEncoder, TextDecoder } from 'util';
 Object.assign(global, { TextDecoder, TextEncoder });*/
+import { setupServer } from 'msw/node';
+import { requestsHandlers } from './src/tests';
 
 /*
   Setup MSW Server for mocking requests during tests
 */
-/*export const server = setupServer(...requestsHandlers);
+export const server = setupServer(...requestsHandlers);
 
 beforeAll(() =>
   server.listen({
@@ -13,4 +15,4 @@ beforeAll(() =>
   })
 );
 afterEach(() => server.resetHandlers());
-afterAll(() => server.close());*/
+afterAll(() => server.close());
