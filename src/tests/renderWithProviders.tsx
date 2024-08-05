22639e22d633ea83c95127dcc3b69d7a510c24f5
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-08-05 13:34:50 +0600

    router-wrapper

--- src/tests/renderWithProviders.tsx
+++ src/tests/renderWithProviders.tsx
@@ -3,24 +3,56 @@ import { render as rtlRender } from '@testing-library/react';
 import { TranslationsProvider } from 'shared/wrappers/TranslationsProvider';
 import { serverStore } from 'store/store';
 import { StoreProvider } from 'shared/wrappers/StoreProvider';
+import {
+  PathnameContext,
+  SearchParamsContext,
+  PathParamsContext,
+} from 'next/dist/shared/lib/hooks-client-context.shared-runtime';
+import {
+  AppRouterContext,
+  AppRouterInstance,
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
-jest.mock('next/navigation', () => {
+const mockRouter: AppRouterInstance = {
+  back: jest.fn(() => Promise.resolve(true)),
+  prefetch: jest.fn(() => Promise.resolve()),
+  push: jest.fn(() => Promise.resolve(true)),
+  replace: jest.fn(() => Promise.resolve(true)),
+  forward: jest.fn(() => Promise.resolve(true)),
+  refresh: jest.fn(() => Promise.resolve(true)),
+
+  /*reload: jest.fn(() => Promise.resolve(true)),
+  beforePopState: jest.fn(() => Promise.resolve(true)),
+
+  basePath: '',
+  pathname: '/',
+  route: '/',
+  query: {},
+  asPath: '/',
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isReady: true,
+  defaultLocale: 'en',
+  domainLocales: [],
+  isPreview: false,*/
+};
+/*jest.mock('next/navigation', () => {
   return {
     __esModule: true,
     useParams: () => ({ locale: 'en' }),
     usePathname: () => '/products',
-    useRouter: () => {
-      return {
-        push: jest.fn(() => Promise.resolve(true)),
-        replace: jest.fn(),
-      };
-    },
+    useRouter: () => mockRouter,
     useSearchParams: () => ({
       get: () => null,
       entries: () => Object.entries({ pageIndex: 0, pageSize: 12 }),
     }),
   };
-});
+});*/
 
 jest.mock('next-intl', () => {
   const messages = require('../../messages/en.json');
@@ -37,11 +69,27 @@ jest.mock('next-intl', () => {
   };
 });
 
-const Wrapper: React.FC<PropsWithChildren> = ({ children }) => {
+const RouterWrapper: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <PathParamsContext.Provider value={{ locale: 'en' }}>
+      <PathnameContext.Provider value={'/products'}>
+        <SearchParamsContext.Provider
+          value={new URLSearchParams('pageSize=12&pageIndex=0')}
+        >
+          <AppRouterContext.Provider value={mockRouter}>
+            {children}
+          </AppRouterContext.Provider>
+        </SearchParamsContext.Provider>
+      </PathnameContext.Provider>
+    </PathParamsContext.Provider>
+  );
+};
+
+const CommonWrapper: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <TranslationsProvider>
       <StoreProvider initialState={{ ...serverStore.getState() }}>
-        {children}
+        <RouterWrapper>{children}</RouterWrapper>
       </StoreProvider>
     </TranslationsProvider>
   );
@@ -49,6 +97,6 @@ const Wrapper: React.FC<PropsWithChildren> = ({ children }) => {
 
 export const renderWithProviders = (ui: React.ReactElement) => {
   return {
-    ...rtlRender(ui, { wrapper: Wrapper }),
+    ...rtlRender(ui, { wrapper: CommonWrapper }),
   };
 };
