d09dff41cc834b9dc19a6823bb63ef5b43c20548
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-09-28 17:09:02 +0600

    refactor

---
+++ src/store/common/slices/queryError.ts
@@ -0,0 +1,51 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootStateType } from 'store/store';
+import type { ResponseApiError } from 'api/baseApi';
+import { endpoints } from 'store/users/query';
+
+type StateType = {
+  error: ResponseApiError<{ message: string }> | null;
+};
+const initialState: StateType = {
+  error: null,
+};
+
+const { actions, reducer } = createSlice({
+  name: 'queryError',
+  initialState,
+  reducers: {
+    reset: () => {
+      return initialState;
+    },
+    set: (state, action: PayloadAction<StateType>) => {
+      state.error = action.payload.error;
+      return state;
+    },
+  },
+  extraReducers: builder => {
+    builder.addMatcher(
+      isAnyOf(endpoints.getUserById.matchRejected),
+      (state, action) => {
+        state.error = action.payload as ResponseApiError<{ message: string }>;
+        /*Object.assign(state, {
+          error: action.payload as ResponseApiError<{ message: string }>,
+        });*/
+      }
+    );
+  },
+});
+
+const selector = {
+  state: (state: RootStateType) => state.common.queryError,
+};
+
+export const queryError = {
+  actions,
+  reducer,
+  selector,
+} as {
+  actions: typeof actions;
+  reducer: typeof reducer;
+  selector: typeof selector;
+};
